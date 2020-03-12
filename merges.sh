0300082cb4f14f9429455d866adc7e5f73da27fd
Anthony Gourraud <anthonygourraud@gmail.com>
2020-03-12 01:08:21 +0100

    fix files merges

--- merges.sh
+++ merges.sh
@@ -1,2 +1,2 @@
-cat ./build-website/www-gen/assets_base/js/*.js> build-website/www-gen/assets_base/scripts.js
-cat ./build-website/www-gen/assets_base/css/*.css> build-website/www-gen/assets_base/styles.css 
+cat ./build-website/www-gen/assets_base/js-to-merge/*.js> build-website/www-gen/assets_base/scripts.js
+cat ./build-website/www-gen/assets_base/css-to-merge/*.css> build-website/www-gen/assets_base/styles.css 
