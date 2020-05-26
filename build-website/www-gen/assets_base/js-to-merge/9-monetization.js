8688c9c015a61b1dfeaa6f2fe72efeb7958a0062
Anthony <a.gourraud@mediameeting.fr>
2020-05-26 16:25:40 +0200

    add multiple payment pointers with probabilistic selection

--- build-website/www-gen/assets_base/js-to-merge/9-monetization.js
+++ build-website/www-gen/assets_base/js-to-merge/9-monetization.js
@@ -2,18 +2,30 @@ let total = 0
 let scale
 let currency
 
+function formatAmount(amount) {
+  return (amount * Math.pow(10, -scale)).toFixed(scale)
+}
+
 if (document.monetization) {
-  document.monetization.addEventListener('monetizationprogress', ev => {
+  document.monetization.addEventListener('monetizationprogress', (ev) => {
     // initialize currency and scale on first progress event
     if (total === 0) {
       scale = ev.detail.assetScale
       currency = ev.detail.assetCode
     }
 
-    total += Number(ev.detail.amount)
+    const actualAmount = Number(ev.detail.amount)
+    total += actualAmount
 
-    const formatted = (total * Math.pow(10, -scale)).toFixed(scale)
-    console.log('Monetization : ', formatted, ' ', currency)
+    const actualFormatted = formatAmount(actualAmount)
+    const totalFormatted = formatAmount(total)
+
+    const pickedPointer = ev.detail.paymentPointer
+
+    console.log(
+      `Monetization : sent ${actualFormatted} ${currency} to ${pickedPointer}.`
+    )
+    console.log(`Total sent for this session : ${totalFormatted}`)
   })
 } else {
   console.log('Monetization : Not activated')
